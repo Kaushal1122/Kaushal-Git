a7812d42103d0fc185e3730e02e567276bb1ec3c
Kaushal Kumar <kk795109@gmail.com>
2024-10-22 21:37:22 +0530

    Stash-commited

--- p3.c
+++ p3.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
-
-// Main function: entry point for execution
 int main() {
-
-    // writing print statement to print hello world
     printf("Hello World");
-
     return 0;
 }
